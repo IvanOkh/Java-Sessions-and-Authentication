3090ea33df7ee7de3ea1f1d808a21fead30edebb
797462 <797462@ELF8IT221427.ACDM.DS.SAIT.CA>
2019-10-03 20:46:54 -0600

    Lab 4 Lab 4 getting close

---
+++ src/java/servlets/LoginServlet.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+import models.AccountService;
+
+/**
+ *
+ * @author 797462
+ */
+public class LoginServlet extends HttpServlet 
+{
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+        {
+            HttpSession session = request.getSession();
+            
+            String ex = request.getParameter("logout");
+            
+            String currentUser = (String) session.getAttribute("currentlyLoged");
+         
+            if (ex != null)
+            {
+                if(currentUser != null)
+                {
+                    response.sendRedirect("Home");
+                }
+                
+                else
+                {
+                    getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+                }
+            }
+            else
+            {
+                session.invalidate();
+                request.setAttribute("inputException", "You have successfully logged out.");
+                getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            }
+            
+        }
+    
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        HttpSession session = request.getSession();//
+        // get the input the user entered
+        String name = request.getParameter("inputOne");//
+        String pass = request.getParameter("inputTwo");//
+        
+        AccountService accServ = new AccountService();
+       
+        User userData = accServ.login(name, pass);
+
+        if (accServ.login(name, pass) == null) 
+        {
+            //display exception warning in ${tag} on login page
+            request.setAttribute("inputException", "User info doesn't"+
+                                                    "match our records");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        } 
+        else 
+        {
+            session.setAttribute("currentlyLoged", userData.getUserName());
+            //head him over to Home page
+            response.sendRedirect("Home");
+        }
+            
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+
+
+        // not used
+    }
+
+}
